d1b9b1c8a7c527a24bc762f20716e11be2f2594d
KVT <kalmonvaladao@gmail.com>
2023-05-15 20:10:11 -0300

    Update p2pt.umd.js
    stun:global.stun.twilio.com:3478

--- dist/p2pt.umd.js
+++ dist/p2pt.umd.js
@@ -9147,7 +9147,7 @@ Peer.config = {
       urls: 'stun:stun.l.google.com:19302'
     },
     {
-      urls: 'stun:global.stun.twilio.com:3478?transport=udp'
+      urls: 'stun:global.stun.twilio.com:3478'
     }
   ],
   sdpSemantics: 'unified-plan'
